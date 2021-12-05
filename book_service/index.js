190313e39462aa1129d9b0561076a5a945738d75
klassman.d <klassman.d@bookcentre.ru>
2021-12-05 13:36:13 +0300

    add socket.io

--- book_service/index.js
+++ book_service/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http');
 const bodyParser = require("body-parser");
 
 // middleware
@@ -11,8 +12,13 @@ const authRouter = require('./routes/api/auth');
 const booksRouterAPI = require('./routes/api/books');
 const booksRouter = require('./routes/books');
 
+// сокет
+const socketIO = require('socket.io');
+
 // зкемпляр приложения
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
 // подключение шаблонизатора ejs
 app.set("view engine", "ejs");
@@ -41,8 +47,53 @@ app.use('/books', booksRouter);
 // ошибки
 app.use(errorMiddleware);
 
+// realtime
+const rooms = {};
+
+io.on('connection', (socket) => {
+    const {id} = socket;
+    console.log(`Socket connected: ${id}`);
+
+    // сообщение себе
+    socket.on('message-to-me', (msg) => {
+        msg.type = 'me';
+        socket.emit('message-to-me', msg);
+    });
+
+    // сообщение для всех
+    socket.on('message-to-all', (msg) => {
+        msg.type = 'all';
+        socket.broadcast.emit('message-to-all', msg);
+        socket.emit('message-to-all', msg);
+    });
+
+    // работа с комнатами
+    const {roomName} = socket.handshake.query;
+    socket.join(roomName);
+    if (roomName) {
+        if (!rooms[roomName]) {
+            rooms[roomName] = { roomName, messages: [] };
+        } else {
+            socket.emit('messages-in-room', { messages: rooms[roomName].messages });
+        }
+    }
+
+    socket.on('message-to-room', (msg) => {
+        msg.type = `room: ${roomName}`;
+        if (roomName) {
+            rooms[roomName].messages.push(msg)
+        };
+        socket.to(roomName).emit('message-to-room', msg);
+        socket.emit('message-to-room', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Socket disconnected: ${id}`);
+    });
+});
+
 // прослушивание порта
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
